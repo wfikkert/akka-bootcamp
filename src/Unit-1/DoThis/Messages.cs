10c83ff12de1b07b62697989940f160fbf091d4e
wfikkert <wfikkert@gmail.com>
2022-12-05 15:36:38 +0100

    Unit 1 - Lesson 2

---
+++ src/Unit-1/DoThis/Messages.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WinTail
+{
+    class Messages
+    {
+        #region Neutral/system messages
+        /// <summary>
+        /// Marker class to continue processing
+        /// </summary>
+        public class ContinueProcessing { }
+        #endregion
+
+        #region Success messages
+        /// <summary>
+        /// Base class for signalling that user input was valid
+        /// </summary>
+        public class InputSuccess
+        {
+            public string Reason { get; private set; }
+            public InputSuccess(string reason)
+            {
+                Reason = reason;
+            }
+        }
+        #endregion
+
+        #region Error messages
+        /// <summary>
+        /// Base class for signalling that user input was invalid
+        /// </summary>
+        public class InputError
+        {
+            public string Reason { get; private set; }
+            public InputError(string reason)
+            {
+                Reason = reason;
+            }
+        }
+
+        /// <summary>
+        /// User provided blank input
+        /// </summary>
+        public class NullInputError : InputError
+        {
+            public NullInputError(string reason) : base(reason) { }
+        }
+
+        /// <summary>
+        /// User provided invalid input (currently, input with odd # characters)
+        /// </summary>
+        public class ValidationError : InputError
+        {
+            public ValidationError(string reason) : base(reason) { }
+        }
+        #endregion
+    }
+}
