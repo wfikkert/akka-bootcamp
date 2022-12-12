666060c99dba76192da529eee854860b4889eb8a
wfikkert <wfikkert@gmail.com>
2022-12-12 09:19:12 +0100

    Unit 2 - Lesson 5

--- src/Unit-2/DoThis/Actors/ChartingActor.cs
+++ src/Unit-2/DoThis/Actors/ChartingActor.cs
@@ -7,7 +7,7 @@ using System.Windows.Forms;
 
 namespace ChartApp.Actors
 {
-    public class ChartingActor : ReceiveActor
+    public class ChartingActor : ReceiveActor, IWithUnboundedStash
     {
         private const int MaxPoints = 250;
         private int xPosCounter = 0;
@@ -62,6 +62,8 @@ namespace ChartApp.Actors
         private Dictionary<string, Series> _seriesIndex;
         private readonly Button _pauseButton;
 
+        public IStash Stash { get; set; }
+
         public ChartingActor(Chart chart, Button pauseButton) : this(chart, new Dictionary<string, Series>(), pauseButton)
         {
         }
@@ -90,12 +92,25 @@ namespace ChartApp.Actors
 
         private void Paused()
         {
+            //stash messages AddSeries and RemoveSeries while paused
+            Receive<AddSeries>(addseries =>
+            {
+                Stash.Stash();
+            });
+            Receive<RemoveSeries>(removeSeries =>
+            {
+                Stash.Stash();
+            });
             Receive<Metric>(metric => HandleMetricsPaused(metric));
             Receive<TogglePause>(pause =>
             {
                 SetPauseButtonText(false);
                 UnbecomeStacked();
+
+                //Unstash the messages when leaving the paused state
+                Stash.UnstashAll();
             });
+            
         }
 
 
